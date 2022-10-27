8792e2411eb126b72b0f9f7a79014389660f4725
rmalegr <rodrigomalegre@gmail.com>
2022-10-26 22:19:21 -0300

    controllers

--- PI-Countries-main/api/src/routes/country.js
+++ PI-Countries-main/api/src/routes/country.js
@@ -7,54 +7,55 @@ const router = Router();
 //SET client_encoding = 'UTF8';
 // localhost:3000/countries
 router.get("/", async (req, res) => {
-  const {name} = req.query;
-  const allCountries = await Country.findAll({
-    include: Activity,
-  });
+	const {name} = req.query;
+	const allCountries = await Country.findAll({
+		include: Activity
+	});
 
-  if (name) {
-    const byName = await allCountries.filter((i) =>
-      i.name.toLowerCase().startsWith(name.toLowerCase()),
-    );
-    byName.length ? res.json(byName) : res.status(404).send({msg: "Not found"});
-  } else {
-    res.json(allCountries);
-  }
+	if (name) {
+		const byName = await allCountries.filter((i) =>
+			i.name.toLowerCase().startsWith(name.toLowerCase())
+		);
+		byName.length ? res.json(byName) : res.status(404).send({msg: "Not found"});
+	} else {
+		res.status(200);
+		res.json(allCountries);
+	}
 });
 
 //GET localhost:3000/countries:id
 
 router.get("/:id", async (req, res, next) => {
-  const {id} = req.params;
-  let countries;
+	const {id} = req.params;
+	let countries;
 
-  try {
-    if (id.length > 1) {
-      countries = await Country.findByPk(id, {include: Activity});
+	try {
+		if (id.length > 1) {
+			countries = await Country.findByPk(id, {include: Activity});
 
-      countries = {
-        id: countries.id,
-        name: countries.name,
-        image: countries.image,
-        continent: countries.continent,
-        capital: countries.capital,
-        subregion: countries.subregion,
-        area: parseInt(countries.area),
-        population: countries.population,
-        activities: countries.activities.map((e) => {
-          return {
-            id: e.id,
-            name: e.name,
-            difficulty: e.difficulty,
-            duration: e.duration,
-            season: e.season,
-          };
-        }),
-      };
-    }
-    res.json(countries);
-  } catch (error) {
-    next(error);
-  }
+			countries = {
+				id: countries.id,
+				name: countries.name,
+				image: countries.image,
+				continent: countries.continent,
+				capital: countries.capital,
+				subregion: countries.subregion,
+				area: parseInt(countries.area),
+				population: countries.population,
+				activities: countries.activities.map((e) => {
+					return {
+						id: e.id,
+						name: e.name,
+						difficulty: e.difficulty,
+						duration: e.duration,
+						season: e.season
+					};
+				})
+			};
+		}
+		res.json(countries);
+	} catch (error) {
+		next(error);
+	}
 });
 module.exports = router;
